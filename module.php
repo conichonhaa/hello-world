9d583b102234d05cc30071fc16c6173fc7702938
conichonhaa <conichonhaa@yahoo.fr>
2025-05-30 08:21:52 +0200

    Create module.php

---
+++ module.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+// Inclure la classe directement
+require_once __DIR__ . '/ApiRestModule.php';
+
+use WebtreesModules\ApiRest\ApiRestModule;
+
+return new ApiRestModule();
