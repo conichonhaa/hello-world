c2681a56d460b1cda56b69d177c9ab9960bf24ec
conichonhaa <conichonhaa@yahoo.fr>
2025-05-30 08:22:48 +0200

    Update module.php

--- module.php
+++ module.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-// Inclure la classe directement
+// Inclure la classe
 require_once __DIR__ . '/ApiRestModule.php';
-
-use WebtreesModules\ApiRest\ApiRestModule;
 
 return new ApiRestModule();
